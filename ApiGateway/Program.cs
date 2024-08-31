c707f664f4820f5ac4174ca70bf5274e94c18116
Ravindra Devrani <ravindra.devrani003@gmail.com>
2024-08-31 21:32:32 +0530

    feat: api gateway added

---
+++ ApiGateway/Program.cs
@@ -0,0 +1,44 @@
+using Ocelot.DependencyInjection;
+using Ocelot.Middleware;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+if (builder.Environment.IsDevelopment())
+{
+    builder.Configuration.AddJsonFile(
+        "ocelot.development.json",
+        optional: false,
+        reloadOnChange: true);
+}
+else
+{
+    builder.Configuration.AddJsonFile("ocelot.production.json", optional: false, reloadOnChange: true);
+}
+
+
+builder.Services.AddOcelot(builder.Configuration);
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.UseOcelot().Wait();
+
+app.MapControllers();
+
+app.Run();
